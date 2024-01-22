a23b6c233d76602f6659ef01e51df7315697d2b5
TuanSimonetti <tuansimonetti@hotmail.com>
2024-01-21 23:44:36 -0300

    Concluse script to export .tgz file, execution bash script and delete exported files

--- install_wifi_dispositive.sh
+++ install_wifi_dispositive.sh
@@ -1,4 +1,8 @@
 cd ./wifi_drive_installer
+tar -xvf iwlwifi-9000-pu-b0-jf-b0-34.618819.0.tgz
+cd ./iwlwifi-9000-pu-b0-jf-b0-34.618819.0
 sudo modprobe -r iwlwifi
 sudo modprobe iwlwifi
-shutdown -r now
+cd ..
+rm -r iwlwifi-9000-pu-b0-jf-b0-34.618819.0
+shutdown -r
